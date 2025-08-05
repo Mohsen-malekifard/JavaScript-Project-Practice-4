5c6c4f1a711aad199bdb1264eb30140cfdf85649
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-05 16:42:47 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const isEven = (num) => num % 2 === 0;
+
+const square = (Num) => {
+    return Num * Num;
+}
+
+const greetUser = (Name) => {
+    return `Welcome ${Name}`;
+}
+
+console.log(isEven(98));
+console.log(square(11));
+console.log(greetUser("Mohsen"));
